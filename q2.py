9b86557e8e3d49b87cb934ba6ba603c7a2ea2350
xunairahbalouch <xunairahbalouch@gmail.com>
2026-01-27 21:55:21 -0800

    Add files via upload

---
+++ q2.py
@@ -0,0 +1,11 @@
+class Vshicle():
+ #PRIVATE ID: STRING
+ #PRIVATE MaxSpeed: INTEGER
+ #PRIVATE IncreaseAmount; INTEGER
+ def __init__(self,IDP, MaxSpeedP, IncreaseAmountIP):
+  self.__ID = IDP
+  self.__MaxSpeed = MaxSpeedP
+  self.__CurrentSpeed = 0
+  self.__HorizontalPosition = 0
+
+
